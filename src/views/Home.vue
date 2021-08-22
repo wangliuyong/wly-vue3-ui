1310a98a12243401fe6dd14981dc00fd63fa2a7f
wly <1355498705@qq.com>
2021-08-22 22:53:21 +0800

    订正名称

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,10 @@
   <div class="topnavAndBanner">
     <Topnav />
     <div class="banner">
-      <h1>轱辘UI</h1>
+      <h1>wly-vue3-ui</h1>
       <h2>一个面向学习者的 UI 框架</h2>
       <p class="actions">
-        <a href="https://github.com">GitHub</a>
+        <a href="https://github.com/wangliuyong/wly-vue3-ui">GitHub</a>
         <router-link to="/doc">开始</router-link>
       </p>
     </div>
